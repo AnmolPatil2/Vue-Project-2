0f92d6e67072cfc4ba2a2fa2484a7c354d7251c3
AnmolPatil2 <anmol.patil004@gmail.com>
2019-05-22 20:42:59 +0530

    before firestore

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="Chat container">
-    <h2>yo {{name}}</h2>
+    <h2 class="center teal-text">Chat-here</h2>
+    <div class="card">
+      <div class="card-content">
+        <ul class="messages">
+          <li>
+            <span class="teal-text">Name</span>
+            <span class="grey-text text-darken-3">messages</span>
+            <span class="gray-text time">time</span>
+          </li>
+        </ul>
+        <div class="card-action">
+          <newMessage :name="name"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import newMessage from "@/components/newMessage.vue";
 export default {
   name: "Chat",
   props: ["name"],
+  components: {
+    newMessage
+  },
   data() {
     return {};
   },
@@ -15,4 +33,18 @@ export default {
 };
 </script>
 <style scoped>
+.Chat h2 {
+  font-size: 2.6em;
+  margin-bottom: 40px;
+}
+.Chat span {
+  font-size: 1.4em;
+}
+.Chat .time {
+  display: block;
+  font-size: 1.4em;
+}
+.messages {
+  text-align: left;
+}
 </style>
---
+++ src/components/newMessage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="new-message">
+    <form @submit.prevent="addmessage">
+      <label for="new-message">Enter your message</label>
+      <input type="text" name="new-message" v-model="newmessage">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Chat",
+  props: ["name"],
+  data() {
+    return { newmessage: null };
+  },
+  methods: {
+    addmessage() {
+      console.log(this.newmessage, this.name, Date.now());
+    }
+  }
+};
+</script>
+<style scoped>
+.Chat h2 {
+  font-size: 2.6em;
+  margin-bottom: 40px;
+}
+.Chat span {
+  font-size: 1.4em;
+}
+.Chat .time {
+  display: block;
+  font-size: 1.4em;
+}
+.messages {
+  text-align: left;
+}
+</style>
